75a4a356899326e613e162328f861bd41010d3ae
so-whatt <115199035+so-whatt@users.noreply.github.com>
2022-10-28 09:23:16 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let x = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     for (let y = 0; y <= 4; y++) {
         for (let x = 0; x <= 4; x++) {
             led.plot(x, y)
@@ -7,21 +9,45 @@ input.onButtonPressed(Button.A, function () {
         }
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    x = -1
+    y = 0
+    for (let index = 0; index < 5; index++) {
+        x += 1
+        led.plot(x, 0)
+        basic.pause(200)
+    }
+    for (let index = 0; index < 4; index++) {
+        y += 1
+        led.plot(x, y)
+        basic.pause(200)
+    }
+    for (let index = 0; index < 4; index++) {
+        x += -1
+        led.plot(x, y)
+        basic.pause(200)
+    }
+    for (let index = 0; index < 4; index++) {
+        y += -1
+        led.plot(x, y)
+        basic.pause(200)
+    }
+})
 input.onButtonPressed(Button.B, function () {
-    x = 0
-    for (let y = 0; y <= 4; y++) {
-        for (let index = 0; index < 5; index++) {
+    basic.clearScreen()
+    x = -1
+    for (let index = 0; index <= 3; index++) {
+        for (let index2 = 0; index2 < 5; index2++) {
+            x += 1
             led.plot(x, y)
             basic.pause(200)
-            x += 1
         }
-        x = 4
         y += 1
-        for (let index = 0; index < 5; index++) {
+        for (let index2 = 0; index2 < 5; index2++) {
             led.plot(x, y)
             basic.pause(200)
             x += -1
         }
-        x = 0
+        y += 1
     }
 })
