51aecae50d01a0ea89a9e5dcfe0f15eefd94fda6
so-whatt <115199035+so-whatt@users.noreply.github.com>
2022-11-06 19:48:58 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,24 +14,11 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
-    x = -1
-    for (let index = 0; index <= 3; index++) {
-        for (let index2 = 0; index2 < 5; index2++) {
-            x += 1
-            led.plot(x, y)
-            basic.pause(200)
-        }
-        y += 1
-        for (let index2 = 0; index2 < 5; index2++) {
-            led.plot(x, y)
-            basic.pause(200)
-            x += -1
-        }
-        y += 1
+    for (let index = 0; index <= 24; index++) {
+        led.plot(Math.abs(Math.abs(index - 4) - 4), index + 5)
+        basic.pause(200)
     }
 })
-let y = 0
-let x = 0
 let list: number[] = []
 let count = 0
 list = [
